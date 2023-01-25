ee1ef0b47807b9e7b4cd34362259218e9e44f310
Andy Chen <totallynotandychen@gmail.com>
2023-01-24 16:26:24 -0800

    item textures and loot table modifications

--- src/main/java/net/fabricmc/liverking/item/ModItems.java
+++ src/main/java/net/fabricmc/liverking/item/ModItems.java
@@ -2,8 +2,6 @@ package net.fabricmc.liverking.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.liverking.LiverKing;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -18,6 +16,7 @@ public class ModItems {
     public static final Item STOMACH = registerItem("stomach", new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(8).saturationModifier(16f).meat().build())));
     public static final Item BRAIN = registerItem("brain", new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(8).saturationModifier(14.4f).meat().build())));
     public static final Item KIDNEY = registerItem("kidney", new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(6).saturationModifier(14.4f).meat().build())));
+    public static final Item HEART = registerItem("heart", new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(4).saturationModifier(16f).meat().build())));
     public static final Item RECONSTRUCTED_ANIMAL = registerItem("reconstructed_animal", new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(20).saturationModifier(20).build()).maxCount(16)));
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(LiverKing.MOD_ID, name), item);
--- src/main/java/net/fabricmc/liverking/util/ModLootModifiers.java
+++ src/main/java/net/fabricmc/liverking/util/ModLootModifiers.java
@@ -3,6 +3,7 @@ package net.fabricmc.liverking.util;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.fabricmc.liverking.item.ModItems;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
@@ -12,22 +13,54 @@ import net.minecraft.util.Identifier;
 
 public class ModLootModifiers {
     private static final Identifier COW_ID = new Identifier("minecraft", "entities/cow");
+    private static final Identifier PIG_ID = new Identifier("minecraft", "entities/pig");
+    private static final Identifier CHICKEN_ID = new Identifier("minecraft", "entities/chicken");
+    private static final Identifier SHEEP_ID = new Identifier("minecraft", "entities/sheep");
+    private static final Identifier RABBIT_ID = new Identifier("minecraft", "entities/rabbit");
+
+    private static final Identifier[] PRIMAL_ANIMALS = {COW_ID, PIG_ID, CHICKEN_ID, SHEEP_ID, RABBIT_ID};
+
+    private static void AddOrgansLootTables(LootTable.Builder tableBuilder) {
+        LootPool.Builder poolBuilder = LootPool.builder()
+                .rolls(ConstantLootNumberProvider.create(1))
+                .conditionally(RandomChanceLootCondition.builder(0.35f))
+                .with(ItemEntry.builder(ModItems.LIVER))
+                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+        tableBuilder.pool(poolBuilder.build());
+        poolBuilder = LootPool.builder()
+                .rolls(ConstantLootNumberProvider.create(1))
+                .conditionally(RandomChanceLootCondition.builder(0.3f))
+                .with(ItemEntry.builder(ModItems.STOMACH))
+                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+        tableBuilder.pool(poolBuilder.build());
+        poolBuilder = LootPool.builder()
+                .rolls(ConstantLootNumberProvider.create(1))
+                .conditionally(RandomChanceLootCondition.builder(0.2f))
+                .with(ItemEntry.builder(ModItems.BRAIN))
+                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+        tableBuilder.pool(poolBuilder.build());
+        poolBuilder = LootPool.builder()
+                .rolls(ConstantLootNumberProvider.create(1))
+                .conditionally(RandomChanceLootCondition.builder(0.25f))
+                .with(ItemEntry.builder(ModItems.KIDNEY))
+                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+        tableBuilder.pool(poolBuilder.build());
+        poolBuilder = LootPool.builder()
+                .rolls(ConstantLootNumberProvider.create(1))
+                .conditionally(RandomChanceLootCondition.builder(0.1f))
+                .with(ItemEntry.builder(ModItems.HEART))
+                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+        tableBuilder.pool(poolBuilder.build());
+    }
 
     public static void ModifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-            if(source.isBuiltin() && COW_ID.equals(id)) {
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.3f))
-                        .with(ItemEntry.builder(ModItems.LIVER))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
-                tableBuilder.pool(poolBuilder.build());
-                poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.3f))
-                        .with(ItemEntry.builder(ModItems.STOMACH))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
-                tableBuilder.pool(poolBuilder.build());
+            if(source.isBuiltin()) {
+                for(Identifier ANIMAL_ID : PRIMAL_ANIMALS) {
+                    if(source.isBuiltin() && ANIMAL_ID.equals(id)) {
+                        AddOrgansLootTables(tableBuilder);
+                    }
+                }
             }
         });
     }
